14b09947b8a7f11f524850bdd9a1c36ac47cf8cf
PhilipHsu609 <mpyhacct118113@gmail.com>
2025-02-12 22:39:30 +0800

    Render african_head.obj

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,38 @@
 #include "tinyrenderer/Drawer.hpp"
+#include "tinyrenderer/Model.hpp"
 #include "tinyrenderer/TGAImage.hpp"
 
-#include <cmath>
+#include <fmt/core.h>
+
+#include <cassert>
 
 int main() {
-    TGAImage img(100, 100, TGAImage::RGB);
+    constexpr int width = 800;
+    constexpr int height = 800;
 
-    TGAColor red(255, 0, 0, 255);
+    TGAImage image(width, height, TGAImage::RGB);
     TGAColor white(255, 255, 255, 255);
 
-    for (int degree = 0; degree <= 180; degree += 5) {
-        const double radian = degree * M_PI / 180.0;
-        const int x0 = static_cast<int>(50 + 40 * std::cos(radian));
-        const int y0 = static_cast<int>(50 + 40 * std::sin(radian));
-        // opposite points of a circle
-        const int x1 = static_cast<int>(50 + 40 * std::cos(radian + M_PI));
-        const int y1 = static_cast<int>(50 + 40 * std::sin(radian + M_PI));
-        line(x0, y0, x1, y1, img, red);
+    Model model("obj/african_head/african_head.obj");
+
+    fmt::print("nverts: {}\n", model.nverts());
+    fmt::print("nfaces: {}\n", model.nfaces());
+
+    for (std::size_t i = 0; i < model.nfaces(); i++) {
+        std::vector<std::size_t> face = model.face(i);
+        for (std::size_t j = 0; j < face.size(); j++) {
+            Vec3f v0 = model.vert(face[j]);
+            Vec3f v1 = model.vert(face[(j + 1) % face.size()]);
+            auto x0 = static_cast<int>((v0.x + 1.f) / 2.f * (width - 1));
+            auto y0 = static_cast<int>((v0.y + 1.f) / 2.f * (height - 1));
+            auto x1 = static_cast<int>((v1.x + 1.f) / 2.f * (width - 1));
+            auto y1 = static_cast<int>((v1.y + 1.f) / 2.f * (height - 1));
+            ::line(x0, y0, x1, y1, image, white);
+        }
     }
 
-    img.flipVertically();
-    img.save("output.tga");
+    image.flipVertically();
+    image.save("output.tga");
 
     return 0;
 }
