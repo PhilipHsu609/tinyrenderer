7d7dbfbe92c0a88daa4eb2876c54a81bceb9f795
PhilipHsu609 <mpyhacct118113@gmail.com>
2025-02-15 23:31:27 +0800

    Back face culling

--- README.md
+++ README.md
@@ -10,3 +10,9 @@ My implementation of [ssloy/tinyrenderer](https://github.com/ssloy/tinyrenderer)
 ## Lesson 1: Bresenham’s Line Drawing Algorithm
 
 - Wireframe rendering
+
+## Lesson 2: Triangle rasterization and back face culling
+
+- Triangle rasterization
+  - Barycentric coordinates
+- Back face culling
--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,51 @@
 #include "tinyrenderer/Drawer.hpp"
 #include "tinyrenderer/Model.hpp"
 #include "tinyrenderer/TGAImage.hpp"
+#include "tinyrenderer/Vector.hpp"
 
 #include <fmt/core.h>
 
+#include <array>
 #include <cassert>
+#include <vector>
 
 int main() {
-    constexpr int width = 200;
-    constexpr int height = 200;
+    constexpr int width = 800;
+    constexpr int height = 800;
 
     TGAImage image(width, height, TGAImage::RGB);
 
-    Vec2i t0[3] = {Vec2i(10, 70), Vec2i(50, 160), Vec2i(70, 80)};
-    Vec2i t1[3] = {Vec2i(180, 50), Vec2i(150, 1), Vec2i(70, 180)};
-    Vec2i t2[3] = {Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180)};
+    Model model("obj/african_head/african_head.obj");
 
-    triangle(t0[0], t0[1], t0[2], image, RED_COLOR);
-    triangle(t1[0], t1[1], t1[2], image, WHITE_COLOR);
-    triangle(t2[0], t2[1], t2[2], image, GREEN_COLOR);
+    fmt::print("nverts: {}\n", model.nverts());
+    fmt::print("nfaces: {}\n", model.nfaces());
+
+    Vec3f light_dir(0.f, 0.f, -1.f);
+
+    for (size_t i = 0; i < model.nfaces(); i++) {
+        std::vector<size_t> face = model.face(i);
+
+        std::array<Vec2i, 3> screen_coords;
+        std::array<Vec3f, 3> world_coords;
+
+        for (size_t j = 0; j < 3; j++) {
+            Vec3f v = model.vert(face[j]);
+            screen_coords[j] = Vec2i(static_cast<int>((v[0] + 1.f) * width / 2.f),
+                                     static_cast<int>((v[1] + 1.f) * height / 2.f));
+            world_coords[j] = v;
+        }
+
+        Vec3f n =
+            (world_coords[2] - world_coords[0]) ^ (world_coords[1] - world_coords[0]);
+        n = n.normalize();
+        float intensity = n * light_dir;
+        if (intensity > 0) {
+            TGAColor color(static_cast<std::uint8_t>(intensity * 255),
+                           static_cast<std::uint8_t>(intensity * 255),
+                           static_cast<std::uint8_t>(intensity * 255));
+            triangle(screen_coords[0], screen_coords[1], screen_coords[2], image, color);
+        }
+    }
 
     image.flipVertically();
     image.save("output.tga");
